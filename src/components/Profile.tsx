ba3b74bcfd3377c15574aff7301127e65ba81ed5
touchgrass83 <aliasdev31@gmail.com>
2025-12-03 13:17:53 +0530

    minor updates

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -81,7 +81,27 @@ const Profile: React.FC<ProfileProps> = ({
                 <div
                   className="image"
                   style={{ backgroundImage: profileImage || undefined }}
-                ></div>
+                >
+                  <div className="edit-image-icon-container">
+                    <svg
+                      className="edit-image-icon w-6 h-6 text-gray-800 dark:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"
+                      />
+                    </svg>
+                  </div>
+                </div>
                 <input
                   type="file"
                   id="imageUpload"
@@ -111,12 +131,12 @@ const Profile: React.FC<ProfileProps> = ({
                 </div>
                 <div className="brutalist-card__message">{user?.email}</div>
                 <div className="brutalist-card__actions">
-                  <a
+                  {/* <a
                     className="brutalist-card__button brutalist-card__button--mark"
                     href="#"
                   >
                     Mark as Read
-                  </a>
+                  </a> */}
                   <a
                     className="brutalist-card__button brutalist-card__button--read"
                     onClick={onLogout}
